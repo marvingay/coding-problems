b538cb51892284a755c5aed3d05f391e16597574
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-13 11:31:28 -0400

    Create MonkAndNiceStrings.cpp

---
+++ HackerEarth/MonkAndNiceStrings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    string arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int v = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                v++;
+            }
+        }
+        cout << v << endl;
+    }
+    return 0;
+}
