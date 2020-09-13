88f00d7da83b31eda283b682754b1de0fb84de8a
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-13 18:21:10 -0400

    Add 4A_Watermelon.cpp

---
+++ CodeForces/4A_Watermelon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int w;
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
