38565933681f72c5a53113763dbabc4bdbdd2499
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-13 19:15:24 -0400

    Add 231A_Team.cpp

---
+++ CodeForces/231A_Team.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b + c >= 2)
+        {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
