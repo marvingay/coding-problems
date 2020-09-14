3c79caac4677c4cab3cc59e2d1161966c14cf9f9
Marvin Gay <MarvinJGay@protonmail.com>
2020-09-13 20:02:06 -0400

    Add 50A_Domino_Piling.cpp

---
+++ CodeForces/50A_Domino_Piling.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+
+    int ans = (m * n) / 2;
+    cout << ans << endl;
+
+    return 0;
+}
